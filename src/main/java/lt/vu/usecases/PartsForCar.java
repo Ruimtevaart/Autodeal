2cc5e2105204622a23ba52d5c5922e4d8e490f93
Ruimtevaart <granata0520@gmail.com>
2022-04-30 11:10:40 +0300

    Added functionality for displaying all available parts for car

---
+++ src/main/java/lt/vu/usecases/PartsForCar.java
@@ -0,0 +1,48 @@
+package lt.vu.usecases;
+
+import lombok.Getter;
+import lombok.Setter;
+import lt.vu.entities.Car;
+import lt.vu.entities.Part;
+import lt.vu.interceptors.LoggedInvocation;
+import lt.vu.persistence.CarsDAO;
+import lt.vu.persistence.PartsDAO;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.util.Map;
+
+@Model
+public class PartsForCar implements Serializable {
+    @Inject
+    private PartsDAO partsDAO;
+
+    @Inject
+    private CarsDAO carsDAO;
+
+    @Getter
+    @Setter
+    private Car car;
+
+    @Getter @Setter
+    private Part partToCreate = new Part();
+
+    @PostConstruct
+    public void init() {
+        Map<String, String> requestParameters =
+                FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        Integer carId = Integer.parseInt(requestParameters.get("carId"));
+        this.car = carsDAO.findOne(carId);
+    }
+
+    @Transactional
+    @LoggedInvocation
+    public void createPart() {
+//    partToCreate.setPart(this.part);
+        partsDAO.persist(partToCreate);
+    }
+}
